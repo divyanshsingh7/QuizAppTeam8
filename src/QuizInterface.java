3795fb8bd7221768530e85b4d4f3356b33e6155b
divyansh.singh <singhdivyansh05100@gmail.com>
2023-10-19 22:27:49 +0530

    first commit

---
+++ src/QuizInterface.java
@@ -0,0 +1,12 @@
+public interface QuizInterface {
+
+    void Login();
+
+    void playQuiz();
+
+    void questions();
+
+    void result();
+
+    void display();
+}
---
+++ src/QuizService.java
@@ -0,0 +1,8 @@
+public class QuizService {
+
+    public void playQuiz() {
+
+        System.out.println("Welcome to the Quiz!");
+        System.out.println("Choose the level:");
+    }
+}
