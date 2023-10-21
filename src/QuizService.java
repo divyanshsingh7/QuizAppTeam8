a8a69db8410f34cd005b2a25163466402afaae93
divyansh.singh <singhdivyansh05100@gmail.com>
2023-10-21 11:21:41 +0530

    Change the structure of the code

--- src/QuizService.java
+++
@@ -1,8 +0,0 @@
-public class QuizService {
-
-    public void playQuiz() {
-
-        System.out.println("Welcome to the Quiz!");
-        System.out.println("Choose the level:");
-    }
-}
---
+++ src/com/quiz/QuizInterface.java
@@ -0,0 +1,14 @@
+package com.quiz;
+
+public interface QuizInterface {
+
+    void login();
+
+    void selectNoOfQuesAndLevel();
+
+    void displayQuesAndTimer();
+
+    void resultValidation();
+
+    void playOnceAgain();
+}
